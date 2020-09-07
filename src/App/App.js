cedde5ee62dd1e9cc2f1d5cae4ee526c5420a9fb
chenzdido <chenzdido@gmail.com>
2020-09-07 15:22:25 +0800

    [zhe] feat: add Studentlist.

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.scss';
+import Studentlist from '../Studentlist/Studentlist';
 
 class App extends Component {
   render() {
     return (
       <div data-testid="app" className="App">
-        Hello World
+        <Studentlist />
       </div>
     );
   }
---
+++ src/Studentlist/Studentlist.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Studentlist extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>学院列表</h2>
+        <button type="button">分组学员</button>
+      </div>
+    );
+  }
+}
+
+export default Studentlist;
